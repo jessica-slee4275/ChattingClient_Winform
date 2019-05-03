a2cb0a4670a6d076e122c0478897f814ffc9c0b1
jessica-slee4275 <jessica.slee4275@gmail.com>
2019-05-02 23:21:15 -0400

    first commit

---
+++ ChattingClient/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace ChattingClient
+{
+    partial class ChattingClient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbChattingHistory = new System.Windows.Forms.ListBox();
+            this.tbText = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.tbIP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbChattingHistory
+            // 
+            this.lbChattingHistory.FormattingEnabled = true;
+            this.lbChattingHistory.Location = new System.Drawing.Point(12, 51);
+            this.lbChattingHistory.Name = "lbChattingHistory";
+            this.lbChattingHistory.Size = new System.Drawing.Size(354, 212);
+            this.lbChattingHistory.TabIndex = 0;
+            // 
+            // tbText
+            // 
+            this.tbText.Location = new System.Drawing.Point(12, 268);
+            this.tbText.Multiline = true;
+            this.tbText.Name = "tbText";
+            this.tbText.Size = new System.Drawing.Size(354, 62);
+            this.tbText.TabIndex = 1;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(372, 24);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 20);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(372, 268);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 62);
+            this.btnSend.TabIndex = 3;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // tbIP
+            // 
+            this.tbIP.Location = new System.Drawing.Point(12, 25);
+            this.tbIP.Name = "tbIP";
+            this.tbIP.Size = new System.Drawing.Size(353, 20);
+            this.tbIP.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "IP Address";
+            // 
+            // ChattingClient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 342);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbIP);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.tbText);
+            this.Controls.Add(this.lbChattingHistory);
+            this.Name = "ChattingClient";
+            this.Text = "Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbChattingHistory;
+        private System.Windows.Forms.TextBox tbText;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox tbIP;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ChattingClient/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace ChattingClient
+{
+    public partial class ChattingClient : Form
+    {
+        Socket client_socket;
+        bool isConnected;
+        byte[] bytes = new byte[1024];
+        string data;
+
+        public ChattingClient()
+        {
+            InitializeComponent();
+            isConnected = false;
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (isConnected == false)
+                return;
+            byte[] msg = Encoding.UTF8.GetBytes(tbText.Text + "<eof>");
+            int bytesSent = client_socket.Send(msg);
+            tbText.Clear();
+            tbText.Text = "";
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (isConnected == true)
+                return;
+            client_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            client_socket.Connect(new IPEndPoint(IPAddress.Parse(tbIP.Text), 7777));
+            lbChattingHistory.Items.Add(String.Format("Socket is Connected."));
+            isConnected = true;
+            Thread listen_thread = new Thread(do_received);
+            listen_thread.Start();
+        }
+        void do_received() {
+            while (isConnected) {
+                while (true) {
+                    byte[] bytes = new byte[1024];
+                    int bytesRec = client_socket.Receive(bytes);
+                    data += Encoding.UTF8.GetString(bytes, 0, bytesRec);
+                    if (data.IndexOf("<eof>") > -1)
+                        break;
+                }
+                data = data.Substring(0, data.Length - 5);
+                Invoke((MethodInvoker)delegate {
+                    lbChattingHistory.Items.Add(data);
+                });
+                data = "";
+            }
+        }
+    }
+}
